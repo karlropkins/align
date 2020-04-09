9a4dc96f4e93c63de0378d3f7196b9edb9d46dc4
karlropkins <karl.ropkins@gmail.com>
2020-04-09 10:29:55 +0100

    unexported code update

---
+++ R/misc_unexported_code.R
@@ -0,0 +1,28 @@
+
+
+
+##################################
+#cow_convert
+##################################
+
+#kr v.0.0.1
+
+#converts cow outputs nSeg and BT
+#into something that can be used
+#by warp_frame
+
+#probably not staying or not staying as is...
+
+cow_convert <- function(nSeg, bT){
+  bT <- as.vector(bT)
+  d <- diff(nSeg)
+
+  ans <- c(nSeg[1])
+  for(i in 1:length(d)){
+    ans <- c(ans,
+             seq(bT[i]+1,bT[i+1], length.out=d[i]))
+  }
+
+  data.frame(x=min(nSeg):max(nSeg),
+             y=ans)
+}
